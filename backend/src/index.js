1f541a93e6c50004c2d3b804100f18fff7a4279b
mucadlucca <mucael@gmail.com>
2020-01-17 11:02:42 -0300

    chore: Add Backend project

---
+++ backend/src/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+const cors = require('cors');
+
+const app = express();
+
+mongoose.connect('mongodb+srv://<username>:<password>@cluster0-ouub0.mongodb.net/week10?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+})
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
